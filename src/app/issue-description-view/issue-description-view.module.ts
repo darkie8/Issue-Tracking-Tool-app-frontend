95588cd8309613eb0cda511f4e08f12e92e8fc80
darkie8 <sdarkie8@gmail.com>
2018-10-20 17:07:21 +0530

    issue-description-view component, search-view component creation

--- src/app/issue-description-view/issue-description-view.module.ts
+++ src/app/issue-description-view/issue-description-view.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IssueDescriptionViewComponent } from './issue-description-view/issue-description-view.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [IssueDescriptionViewComponent]
 })
 export class IssueDescriptionViewModule { }
--- src/app/search-view/search-view.module.ts
+++ src/app/search-view/search-view.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchViewComponent } from './search-view/search-view.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [SearchViewComponent]
 })
 export class SearchViewModule { }
