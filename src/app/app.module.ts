723ea8547268e5d2814fb2ba5cabf0b55aada04e
darkie8 <sdarkie8@gmail.com>
2018-10-21 10:05:04 +0530

    app-module and user-entry module updatation with needed modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchViewModule } from './search-view/search-view.module';
+import { UserEntryModule } from './user-entry/user-entry.module';
+import { SpecialSelectorsModule } from './special-selectors/special-selectors.module';
+import { IssueDescriptionViewModule } from './issue-description-view/issue-description-view.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './user-entry/login/login.component';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    SearchViewModule,
+    UserEntryModule,
+    SpecialSelectorsModule,
+    IssueDescriptionViewModule,
+    DashboardModule,
+    RouterModule.forRoot([{path: 'login' , component: LoginComponent, pathMatch: 'full'},
+    { path: '', component: LoginComponent },
+    { path: '*', component: LoginComponent }], {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-entry/user-entry.module.ts
+++ src/app/user-entry/user-entry.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastModule } from 'ng6-toastr';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ToastModule.forRoot(),
+    RouterModule.forChild([{path: 'register', component: RegisterComponent}])
   ],
   declarations: [LoginComponent, RegisterComponent]
 })
