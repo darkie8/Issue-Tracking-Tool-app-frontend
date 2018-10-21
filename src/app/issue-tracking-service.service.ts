97107847e607ca823cc824abd4c66b21edcb68f3
darkie8 <sdarkie8@gmail.com>
2018-10-21 17:43:05 +0530

    register method created

--- src/app/issue-tracking-service.service.ts
+++ src/app/issue-tracking-service.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpHeaders } from '@angular/common/http';
 
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/zip';
+import 'rxjs/add/operator/toPromise';
+import { Cookie } from '../../node_modules/ng2-cookies/ng2-cookies';
 @Injectable({
   providedIn: 'root'
 })
 export class IssueTrackingServiceService {
+  private url = '';
 
-  constructor() { }
+  constructor(private httpCall: HttpClient) { }
+
+  // registering
+  public registeringMethod(data): Observable<any> {
+    const Body = {firstName: data.firstName,
+      lastName: data.lastName,
+      mobile: data.mobile,
+      email: data.email,
+      password: data.password};
+      return this.httpCall.post(`${this.url}/api/v1/users/register`, Body);
+  }
+
+
+
+
+
+  // handling error
+  private handleError(err: HttpErrorResponse) {
+
+    let errorMessage = '';
+
+    if (err.error instanceof Error) {
+
+      errorMessage = `An error occurred: ${err.error.message}`;
+
+    } else {
+
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+
+    } // end condition *if
+
+    console.error(errorMessage);
+
+    return Observable.throw(errorMessage);
+
+  }  // END handleError
+
 }
--- src/app/user-entry/register/register.component.ts
+++ src/app/user-entry/register/register.component.ts
@@ -9,9 +9,75 @@ import { IssueTrackingServiceService } from 'src/app/issue-tracking-service.serv
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(service: IssueTrackingServiceService) { }
-
+  constructor(
+    private service: IssueTrackingServiceService,
+    private router: Router,
+    private toastr: ToastsManager,
+    private vcr: ViewContainerRef) {
+    toastr.setRootViewContainerRef(vcr);
+  }
+  public firstName: any;
+  public lastName: any;
+  public mobile: any;
+  public email: any;
+  public password: any;
   ngOnInit() {
   }
+
+  // go to login page
+  goToLogin: any = () => {
+    this.router.navigate(['/login']);
+  }
+
+  // registering user
+
+  signUp: any = () => {
+    if (!this.firstName) {
+      this.toastr.warning('Firstname missing');
+
+
+    } else if (!this.lastName) {
+      this.toastr.warning('Lastname missing');
+
+    } else if (!this.mobile) {
+      this.toastr.warning('Mobile number missing');
+
+    } else if (!this.email) {
+      this.toastr.warning('Email missing');
+
+    } else if (!this.password) {
+      this.toastr.warning('Password missing');
+    } else {
+      const data = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        mobile: this.mobile,
+        email: this.email,
+        password: this.password
+      };
+
+      console.log(data);
+      this.service.registeringMethod(data).subscribe(
+        // if user data is fetched
+        success => {
+          console.log(success);
+          if (success.status === 200) {
+            this.toastr.success('registering successful');
+            setImmediate(() => this.toastr.success(`A verification message has been sent to ${data.email}`));
+            setTimeout(() => {
+              this.goToLogin();
+            }, 2000);
+          } else {
+            this.toastr.error(success.message);
+          }
+        },
+        // if user data can not be fetched
+        error => {
+          this.toastr.warning('can not be registered');
+        }
+      );
+    }
+  }
+  // registering method ends
 
 }
