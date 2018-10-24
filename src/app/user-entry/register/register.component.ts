f132c015ce27dece755bf13a27b8cb9d49365015
darkie9 <sdarkei9@gmail.com>
2018-10-24 16:04:37 +0530

    check

--- src/app/user-entry/register/register.component.ts
+++ src/app/user-entry/register/register.component.ts
@@ -3,6 +3,7 @@ import { Router } from '../../../../node_modules/@angular/router';
 import { ToastsManager } from '../../../../node_modules/ng6-toastr';
 import { IssueTrackingServiceService } from 'src/app/issue-tracking-service.service';
 @Component({
+  
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
